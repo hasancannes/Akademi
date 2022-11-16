bafcec80fa093d9b6eaf705cd2abc35f14aed05a
hasancannes <hasancannesanir@gmail.com>
2022-11-16 18:05:39 +0300

    ilk

---
+++ main.py
@@ -0,0 +1,65 @@
+# print fonksiyonu
+print("Merhaba Etiya")
+# string
+text = 'Hello'
+print(text)
+# integer
+number = 5
+print(number)
+# float
+dnumber = 5.3
+print(dnumber)
+# boolean
+isverified = True
+print(isverified)
+# matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 5)
+# mantıksal operatörler
+print(1==2)
+print(1!=2)
+print(1>2)
+print(1<2)
+print(1<=2)
+print(1>=2)
+print(10%2==0)
+print(50/2==25)
+print(50/2==25.0)
+# string fonksiyonları
+text = 'Merhaba Etiya'
+print(text.upper())
+print(text.lower())
+print(text.startswith("M"))
+print(text.endswith("Etiya"))
+# f string
+name = 'Hasan'
+age = 26
+company = 'Etiya'
+print(f"{name} {age} yasinda ve {company}'de calisiyor")
+
+
+# kodlama.io
+baslik = "Haberiniz Olsun"
+vade = 20
+faizOrani1 = 1.50
+faizOrani2 = 1.44
+print(baslik)
+print(type(baslik))
+print(vade)
+print(type(vade))
+print(faizOrani1)
+print(type(faizOrani1))
+
+mesaj = 'Hoşgeldin'
+musteriAdi = 'Hasan'
+musteriSoyadi = 'Can'
+sonucMesaj = mesaj + " " + musteriAdi + " " + musteriSoyadi
+print(f"{mesaj} {musteriAdi} {musteriSoyadi}")
+print(sonucMesaj)
+
+sayi1 = 100
+sayi2 = 50
+print(sayi1 + sayi2)

9fa4f7153c653d1d0896d4a55c0bd6889b10b295
hasancannes <hasancannesanir@gmail.com>
2022-11-17 13:18:34 +0300

    secondcommit

---
+++ Kodlama.io(ders ve ödevler)/degiskenler.py
@@ -0,0 +1,22 @@
+# degiskenler
+baslik = "Haberiniz Olsun"
+vade = 20
+faizOrani1 = 1.50
+faizOrani2 = 1.44
+print(baslik)
+print(type(baslik))
+print(vade)
+print(type(vade))
+print(faizOrani1)
+print(type(faizOrani1))
+
+mesaj = 'Hoşgeldin'
+musteriAdi = 'Hasan'
+musteriSoyadi = 'Can'
+sonucMesaj = mesaj + " " + musteriAdi + " " + musteriSoyadi 
+print(f"{mesaj} {musteriAdi} {musteriSoyadi}")
+print(sonucMesaj)
+
+sayi1 = 100
+sayi2 = 50
+print(sayi1 + sayi2)

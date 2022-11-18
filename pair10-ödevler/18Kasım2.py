d52345360f28fa15d1bdf856fe45df0797d62553
hasancannes <hasancannesanir@gmail.com>
2022-11-18 15:17:13 +0300

    ödev2

--- pair10-ödevler/18Kasım2.py
+++ pair10-ödevler/18Kasım2.py
@@ -6,27 +6,33 @@
 # Programın sonunda bu dosyadan bilgileri satır satır okuyup listeleyecek kod yazılacak.
 # Eklenen çalışanlar silinmeyecek üzerine yazılacak.
 
-employeesCount = int(input("Şirketinizin Çalişan Sayisini Giriniz: "))
+
 try:
-    for i in range(1,employeesCount+1):
-        try:
-            name = str(input(f"{i}. Çalisaninizin Adini Giriniz: "))
-            surname = str(input(f"{i}. Çalisaninizin SoyAdini Giriniz: "))
-            salary = float(input(f"{i}. Çalisaninizin Maas Bilgisini Giriniz: "))
-        except:
-            print("Lütfen Sayi Giriniz")
-        file = open("employees.txt","a+")
-        try:
-            file.writelines(f"{name} {surname} - {salary}\n")
-        except:
-            print("Hatali giriş tespit edildi.")
-    file = open("employees.txt","r")
-    line = file.readlines()
-    valueList = []
-    for i in line:
-        clearValues = i.rstrip("\n")
-        valueList.append(clearValues)
-    print(valueList)
-    file.close()
+    employeesCount = int(input("Şirketinizin Çalişan Sayisini Giriniz: "))
+    if employeesCount>0:
+    
+        for i in range(1,employeesCount+1):
+            try:
+                name = str(input(f"{i}. Çalisaninizin Adini Giriniz: "))
+                surname = str(input(f"{i}. Çalisaninizin SoyAdini Giriniz: "))
+                salary = float(input(f"{i}. Çalisaninizin Maas Bilgisini Giriniz: "))
+            except:
+                print("Lütfen Sayi Giriniz")
+            file = open("employees.txt","a+")
+            try:
+                file.writelines(f"{name} {surname} - {salary}\n")
+            except:
+                print("Hatali giriş tespit edildi.")
+        file = open("employees.txt","r")
+        line = file.readlines()
+
+        valueList = []
+        for i in line:
+            clearValues = i.rstrip("\n")
+            valueList.append(clearValues)
+        print(valueList)
+        file.close()
+    else:
+        print("En az calisan sayiniz 1 olmalidir.")
 except:
-    print("En az bir calisaniniz olmasi gerekmektedir.")
+    print("Yanlis değer girdiniz.")
